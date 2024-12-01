8b12210440bfb9e3b8f7a9bf86c109e4b7f7276c
Marc Kulshrestha <marc.pgdm25g@greatlakes.edu.in>
2024-12-01 15:20:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ class StockPredictor:
         self.data = None
         self.model = None
         self.model_fit = None
-        self.predictions = None
+        #self.predictions = None
         self.metrics = {}
         self.var_metrics = {}
 
